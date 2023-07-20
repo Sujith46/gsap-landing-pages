64120d1850a88076586ff5eb1a632401ab873062
Sujith46 <sujithsuresh246@gmail.com>
2023-07-20 15:46:02 +0530

    feat: json data for distort page

---
+++ src/constants/distort/cars.js
@@ -0,0 +1,66 @@
+import Car_One from '../../assets/images/distort/car_1.jpg'
+import Car_Two from '../../assets/images/distort/car_2.jpg'
+import Car_Three from '../../assets/images/distort/car_3.jpg'
+import Car_Four from '../../assets/images/distort/car_4.jpg'
+import Car_Five from '../../assets/images/distort/car_5.jpg'
+
+
+
+export const cars = {
+  sports_cars: [
+    {
+      name: "Ferrari 488 GTB",
+      description:
+        "The Ferrari 488 GTB is a mid-engine supercar known for its exceptional performance and aerodynamic design.",
+      image: Car_One,
+      heading: "Ferrari 488 GTB",
+      subheading: "Unleash the Beast",
+      manufactur_year: 2015,
+    },
+    {
+      name: "Porsche 911 Turbo S",
+      description:
+        "The Porsche 911 Turbo S is an iconic sports car with all-wheel drive and a powerful turbocharged engine.",
+      image: Car_Two,
+      heading: "Porsche 911 Turbo S",
+      subheading: "Precision Engineering",
+      manufactur_year: 2020,
+    },
+    {
+      name: "Lamborghini Huracán Performante",
+      description:
+        "The Lamborghini Huracán Performante is a high-performance sports car with advanced aerodynamics and cutting-edge technology.",
+      image: Car_Three,
+      heading: "Lamborghini Huracán Performante",
+      subheading: "Unbridled Power",
+      manufactur_year: 2017,
+    },
+    {
+      name: "Audi R8 V10 Plus",
+      description:
+        "The Audi R8 V10 Plus is a luxurious sports car that combines elegant design with impressive power and handling.",
+      image: Car_Three,
+      heading: "Audi R8 V10 Plus",
+      subheading: "The Ultimate Expression of Performance",
+      manufactur_year: 2019,
+    },
+    {
+      name: "McLaren 720S",
+      description:
+        "The McLaren 720S is a lightweight supercar with a twin-turbo V8 engine, designed for ultimate speed and agility.",
+      image: Car_Four,
+      heading: "McLaren 720S",
+      subheading: "Born to Perform",
+      manufactur_year: 2018,
+    },
+    {
+      name: "Aston Martin DBS Superleggera",
+      description:
+        "The Aston Martin DBS Superleggera is a grand tourer with a muscular design and a potent V12 engine.",
+      image: Car_Five,
+      heading: "Aston Martin DBS Superleggera",
+      subheading: "Elegant Powerhouse",
+      manufactur_year: 2019,
+    },
+  ],
+};
