d6ef61063d1e691e41434c4f475fd21958327133
Sujith46 <sujithsuresh246@gmail.com>
2023-07-21 18:42:26 +0530

    fix: add backtohome component custom styles

--- src/app/animated-cursor/page.js
+++ src/app/animated-cursor/page.js
@@ -1,16 +1,18 @@
 'use client'
 
 import { AnimatedCursorNav } from "@/components/AnimatedCursor/AnimatedCursorNav";
+import { BackToHome } from "@/components/common/backToHome/BackToHome";
 import { usePathname } from "next/navigation";
 
 export default function AnimatedCursor() {
-    
+
     if(window) {
      document.querySelector('html').style.cursor = "none"
     }
 
     return (
         <main>
+            <BackToHome style="py-4"/>
             <AnimatedCursorNav />
         </main>
     )
--- src/components/common/backToHome/BackToHome.js
+++ src/components/common/backToHome/BackToHome.js
@@ -1,10 +1,13 @@
-import Link from "next/link"
-import { ArrowLeft } from "react-feather"
+import Link from "next/link";
+import { ArrowLeft } from "react-feather";
 
-export const BackToHome = () => {
-    return (
-        <Link href={"/"} className="text-gray-400 flex items-center justify-center gap-2 hover:text-gray-100 ease-in-out duration-200">
-            <ArrowLeft /> Back to Home page
-        </Link>
-    )
-}
+export const BackToHome = ({ style }) => {
+  return (
+    <Link
+      href={"/"}
+      className={`${style} text-gray-400 flex items-center justify-center gap-2 hover:text-gray-100 ease-in-out duration-200`}
+    >
+      <ArrowLeft /> Back to Home page
+    </Link>
+  );
+};
